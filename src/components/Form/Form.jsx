3c8390a742233c785ad69e0c48089f716c80a3e9
Dmitry Pasko <pasko335@gmail.com>
2023-12-12 22:15:01 +0200

    New functionality for tasks

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,36 +1,47 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
-import { addNewTask } from '../../redux/tasksSlice';
-
+import { addNewTask, clearTasks } from '../../redux/tasksSlice';
+import { nanoid } from 'nanoid'
 
 const Form = () => {
   const dispatch = useDispatch();
-  //const tasks = useSelector(state => state.tasks);
   const [formValue, setFormValue] = useState('');
 
-  const hendleAddTask = (evt) => {
+  const handleAddTask = (evt) => {
     evt.preventDefault();
     setFormValue(evt.target.value)
   }
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    dispatch(addNewTask(formValue));
+
+    const newTask = {
+      id: nanoid(5),
+      task: formValue,
+      status: false
+    }
+    setFormValue('');
+    dispatch(addNewTask(newTask));
+  };
+
+  const handleClearTasks = () => {
+    dispatch(clearTasks());
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input 
+        <input
           type='text'
           name='task'
           value={formValue}
           placeholder='Enter your task...'
-          onChange={hendleAddTask}
+          onChange={handleAddTask}
         />
 
         <button type='submit'>Add Task</button>
       </form>
+      <button onClick={handleClearTasks} type='submit'>Clean Tasks</button>
     </div>
   )
 }
--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -1,17 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const formDatailsSlice = createSlice({
-    name: 'tasks',
-    initialState: [],
-    reducers: { 
-      addNewTask: (state, action) => {
-          return [...state, action.payload];
-      },
-      clearTasks: (state) => {
-        return [];
-      },
-    }
+  name: 'tasks',
+  initialState: [],
+  reducers: {
+    addNewTask: (state, action) => {
+      return [...state, action.payload];
+    },
+    clearTasks: (state) => {
+      return [];
+    },
+    taskStatus: (state, action) => {
+      for (const task of state) {
+        if (task.id === action.payload) {
+          task.status = !task.status;
+          break;
+        }
+      }
+    },
+    deleteTask: (state, action) => {
+      const index = state.findIndex(task => task.id === action.payload);
+      state.splice(index, 1);
+    },
+  },
 });
 
-export const { addNewTask, clearTasks } = formDatailsSlice.actions;
+export const { addNewTask, clearTasks, taskStatus, deleteTask } = formDatailsSlice.actions;
 export const formDatailsReducer = formDatailsSlice.reducer;
